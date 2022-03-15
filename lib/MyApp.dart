35a419dc3661f3a090c7958a853726b0adf33359
KidProf <bitballoon2018@gmail.com>
2022-03-15 09:21:03 +0000

    lock orientation to protrait

--- lib/MyApp.dart
+++ lib/MyApp.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 import 'package:fluttertoast/fluttertoast.dart';
 
@@ -25,6 +26,10 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown,
+    ]);
     return const MaterialApp(
       title: _title,
       home: MyStatefulWidget(),
